3a75b123df290a1c94fbc5a78c9f2f2fd6add80a
Brandon205 <brandonblack02@gmail.com>
2019-09-26 14:35:29 -0700

    Laid out HTML and CSS

--- bomb.js
+++ bomb.js
@@ -1,1 +1,26 @@
-console.log('loaded!');
+//Global Variables 
+const STARTING_TIME = 30;
+var remainingTime = 0;
+var gameOver = false;
+var countdown = null;
+var delay = null;
+var wireState = {
+    blue: false,
+    green: false, 
+    red: false,
+    white: false,
+    yellow: false
+}
+
+var wiresToCut = [];
+
+//DOM References TO-DO: audio 
+var timer = document.getElementById('timer');
+var wireBox = document.getElementById('wirebox');
+var resetButton = document.getElementById('button');
+
+//Event Handlers
+
+
+//Other Functions
+
